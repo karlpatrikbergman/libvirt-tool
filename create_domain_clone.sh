c8a09aeaf0b29ed98d1940bea7e201b9f0b66252
Patrik Bergman <patrik.bergman@infinera.com>
2017-06-20 16:42:20 +0200

    Added run example

--- create_domain_clone.sh
+++ create_domain_clone.sh
@@ -5,7 +5,17 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 set -x
 
 # REMEMBER:
-# There must be a domain "centos7_clone_source" on the node for this script to work.
+# There must be a domain "centos7_clone_source" in directory "/var/lib/libvirt/images" on the node for this script to
+# work.
+#
+# Example:
+# ./create_domain_clone.sh qemu+ssh://root@tnm-vm7/system pabe_test /var/lib/libvirt/image
+#
+# TODO: Add set hostname. Copied from Niclas bash script
+# echo "Trying to set hostname to $name in VM"
+# ping -c1 tnm-centos7 && sshpass -v -p$SSHPASSWORD ssh -v root@tnm-centos7 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "hostnamectl set-hostname $name && reboot" && echo "Set hostname to $name"
+
+
 
 CONNECTION=${1}
 DOMAIN=${2}
